ef0cae3bfe2da0979a01c214f4a49cd87681c5d0
gangand <dmitriigangan.reg@gmail.com>
2017-12-26 20:43:52 +0200

    New branch 1st commit

--- soap_sandbox.py
+++ soap_sandbox.py
@@ -10,31 +10,32 @@ password='manager'
 session = Session()
 session.auth = HTTPBasicAuth(user, password)
 
-#Path to WDSL file should be indicated
+
 client = Client('cpeService.xml',transport=Transport(session=session))
 
 mg_id=2
-t_id=8004
-v_id=24
-request_data = {
-    'cpeId': {
+t_id=1002
+l_idx=0
+request_data = {|
+    'id': {
         'managedGroupId': mg_id,
         'subscriberId': t_id,
-    },
-	'vlanIdToUpdate':v_id,
-    'backhauling': {
-        'name': 'ICMP',
-        'cpeSideIPAddressSource': 'PROFILE',
-    }}
+    },}
+
+request_data2 = {
+        'managedGroupId': mg_id,
+        'lastIndex': l_idx
+        }
 a=input('Add/Delete?: ')
 try:
-    if a=='d':
-        response = client.service.cpeDeleteBHfromVR(**request_data)
-    elif a=='a':
-        response = client.service.cpeAddBHtoVR(**request_data)
+    response = client.service.getCPEbyID(**request_data)
+    response2 = client.service.getCPEsByManagedGroup(**request_data2)
 except exceptions.Fault as error:
         print (error.message)
 except requests.exceptions.ConnectionError as http_error:
     print('Connection problem...')
-else: print('Successfuly {} BH:{} from VSAT:{} in VLAN:{}'.format(a,mg_id,t_id,v_id))
+else: print('Successfuly:{} '.format(t_id))
+if response:
+    print (response['vrs']['VR'][0]['vlanId'])
+    print (response2)
 print ('\n')
